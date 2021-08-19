09668c0187e52e0170fb8379fb02a800a3f2892d
Prajwal Kadam <81584701+imPrajwalKadam@users.noreply.github.com>
2021-08-19 17:13:26 +0530

    Add files via upload

---
+++ srcIfBlock/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<div style="text-align:center"><h1>Angular Cha Abhyas : ngIf Demo</h1><app-ng-if-demo></app-ng-if-demo></div>',
+})
+export class AppComponent {
+  title = 'app';
+}
