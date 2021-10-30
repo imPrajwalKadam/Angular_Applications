b6f7e7712dcf0822b5bae38bd5adbe58cc607c32
Prajwal Kadam <prajwalkadam03@gmail.com>
2021-10-30 22:58:29 +0530

    this folder contains services topic in angular

---
+++ services_in_angular/src2/app/marvellous.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MarvellousService {
+  constructor() { }
+  fun()
+  {
+    return "Atharva";
+  }
+}
